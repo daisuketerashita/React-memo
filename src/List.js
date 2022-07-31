a58117849a7a81ea59dffc39ac67dc8bde0c9782
寺下大典 <ekusiad.tera@icloud.com>
2022-07-31 22:08:09 +0900

    親子間のデータのやり取り

--- src/List.js
+++ src/List.js
@@ -1,16 +1,8 @@
-const LANGUAGES = [
-    'JavaScript',
-    'C++',
-    'Ruby',
-    'PHP',
-    'Go'
-];
-
-export const List = () => {
+export const List = ({langs}) => {
     return (
         <div>
             {
-                LANGUAGES.map((lang,index) => {
+                langs.map((lang,index) => {
                     return <div key={index}>{lang}</div>
                 })
             }

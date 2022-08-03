86839da0d4e2b61040f07d417278bcd87931bd53
寺下大典 <ekusiad.tera@icloud.com>
2022-08-03 22:14:56 +0900

    refを使ったfocus

--- src/Hint.js
+++ src/Hint.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useState,useRef,useEffect } from 'react';
 
 const HintContainer = styled.div`
 position: relative;
@@ -17,12 +18,37 @@ height: 24px;
 cursor: pointer;
 `
  
+const PopupContainer = styled.div`
+position: absolute;
+left: 88%;
+bottom: 12px;
+display: flex;
+justify-content: center;
+border: 1px solid black;
+border-radius: 8px;
+padding: 8px;
+width: 140px;
+`
+
 export const Hint = () => {
+    const [showPopup,setShowPopup] = useState(false);
+
+    const ref = useRef(null);
+
+    useEffect(() => {
+        if(ref.current) ref.current.focus();
+    })
+
     return (
         <HintContainer>
-            <HintInner>
+            <HintInner onClick={() => setShowPopup(true)}>
                 ?
             </HintInner>
+            {
+                showPopup && (
+                    <PopupContainer ref={ref} onBlur={() => setShowPopup(false)} tabIndex={0}>言語の名前です</PopupContainer>
+                )
+            }
         </HintContainer>
     )
 }

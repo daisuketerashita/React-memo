3572f812f3b79497365ac752bec1f3f6380b43e7
寺下大典 <ekusiad.tera@icloud.com>
2022-08-03 22:49:17 +0900

    CSS調整

--- src/App.js
+++ src/App.js
@@ -5,26 +5,9 @@ import { Form } from "./Form";
 import { Modal } from './components/modal';
 import { getLanguages } from './const/languages';
 import { withLoading } from './hoc/withLoading';
+import { Header } from './Header';
 
-const Header = styled.header`
-display: flex;
-justify-content: spece-between;
-padding: 24px 64px 0px;
-border-bottom: 1px solid #E0E0E0;
-`
 
-const HeaderUl = styled.ul`
-display: flex;
-margin: 0;
-padding: 0;
-`
-
-const HeaderLi = styled.li`
-list-style: none;
-padding: 4px 12px;
-cursor: pointer;
-border-bottom: ${props => props.focused ? '2px solid #F44336' : 'none'}
-`
 
 function App({data}) {
   const [tab,setTab] = useState('list');
@@ -37,12 +20,7 @@ function App({data}) {
 
   return (
     <div>
-      <Header>
-        <HeaderUl>
-          <HeaderLi focused={tab === 'list'} onClick={() => setTab('list')}>リスト</HeaderLi>
-          <HeaderLi focused={tab === 'form'} onClick={() => setTab('form')}>フォーム</HeaderLi>
-        </HeaderUl>
-      </Header>
+      <Header tab={tab} setTab={setTab} />
       {
         tab === 'list' ? <List langs={langs}/> : <Form onAddLang={addLang} />
       }
